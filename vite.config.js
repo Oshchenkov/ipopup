44dd5f49ac7621c6809b0ff29b68208c35fd2106
Oshchenkov <oshchenkovv@gmail.com>
2023-04-26 23:39:15 +0300

    setup lib build vite

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,27 @@
-import { defineConfig } from 'vite'
+import { defineConfig } from 'vite';
+import { resolve } from 'path';
 
-/** @type {import('vite').UserConfig} */
-export default defineConfig({
-  // ...
-})
+export default defineConfig(() => {
+    const dir = resolve(__dirname, 'counter.js');
+    console.log("-> dir", dir);
+    return {
+        build: {
+            lib: {
+                // Could also be a dictionary or array of multiple entry points
+                entry: resolve(__dirname, 'src/counter.ts'),
+                name: 'MyLib',
+                // the proper extensions will be added
+                fileName: 'my-lib',
+            },
+            rollupOptions: {
+                // make sure to externalize deps that shouldn't be bundled
+                // into your library
+                output: {
+                    // Provide global variables to use in the UMD build
+                    // for externalized deps
+                    globals: {},
+                },
+            },
+        },
+    };
+});
