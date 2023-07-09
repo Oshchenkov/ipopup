7da0359c96e630687ccaa479bd1cde22eed07fbd
Oshchenkov <oshchenkovv@gmail.com>
2023-07-09 16:19:34 +0300

    dsf

--- src/types/index.types.ts
+++ src/types/index.types.ts
@@ -1,11 +1,11 @@
-export interface IOptions {
-  type: "defaultPopup" | "defaultPopup2";
-}
-
 export interface IPopupContainer {
   state: Map<string, IPopupInstance>;
 }
 
 export interface IPopupInstance {
   id: string;
+}
+
+export interface IOptions {
+  type: "defaultPopup" | "defaultPopup2";
 }
